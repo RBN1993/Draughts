9c933eb8c1feeec671247e7f64bbfa4e79604119
rbn1993 <surfacerobin@outlook.com>
2020-04-26 22:22:36 +0200

    [Remove random piece]:rename methods and extract to a method store coordinates

--- draughts/src/main/java/es/urjccode/mastercloudapps/adcs/draughts/models/Game.java
+++ draughts/src/main/java/es/urjccode/mastercloudapps/adcs/draughts/models/Game.java
@@ -41,9 +41,9 @@ public class Game {
             error = this.isCorrectPairMove(pair, coordinates);
             if (error == null) {
                 List<Coordinate> possibleCoordinatesToRemove = new ArrayList<Coordinate>();
-                this.foundPossibleCoordinates(removedCoordinates, pair, possibleCoordinatesToRemove, coordinates);
+                this.foundPossibleCoordinatesToEat(removedCoordinates, pair, possibleCoordinatesToRemove, coordinates);
                 this.pairMove(removedCoordinates, pair, coordinates);
-                this.removeWhenItWasPossibleToEatBefore(removedCoordinates, possibleCoordinatesToRemove);
+                this.removePieceWhenItWasPossibleToEatBefore(removedCoordinates, possibleCoordinatesToRemove);
                 pair++;
             }
         } while (pair < coordinates.length - 1 && error == null);
@@ -55,7 +55,7 @@ public class Game {
         return error;
     }
 
-    private void removeWhenItWasPossibleToEatBefore(List<Coordinate> removedCoordinates, List<Coordinate> possibleCoordinatesToRemove) {
+    private void removePieceWhenItWasPossibleToEatBefore(List<Coordinate> removedCoordinates, List<Coordinate> possibleCoordinatesToRemove) {
         if (removedCoordinates.size() == 0) {
             if (possibleCoordinatesToRemove.size() == 1) {
                 removedCoordinates.add(0, possibleCoordinatesToRemove.get(0));
@@ -69,19 +69,23 @@ public class Game {
         }
     }
 
-    private void foundPossibleCoordinates(List<Coordinate> removedCoordinates, int pair, List<Coordinate> possibleCoordinatesToRemove, Coordinate[] coordinates) {
+    private void foundPossibleCoordinatesToEat(List<Coordinate> removedCoordinates, int pair, List<Coordinate> possibleCoordinatesToRemove, Coordinate[] coordinates) {
         if (removedCoordinates.size() == 0) {
             List<Coordinate> coordinatesWithActualColor = this.getCoordinatesWithActualColor();
             for (Coordinate coordinateWithSameColor : coordinatesWithActualColor)
                 if (this.getPiece(coordinateWithSameColor) != this.getPiece(coordinates[pair])) {
                     List<Coordinate> diagonalCoordinates = coordinateWithSameColor.getDiagonalCoordinates(2);
-                    for (Coordinate targetCoordinate : diagonalCoordinates)
-                        if (this.isCorrectPairMove(0, coordinateWithSameColor, targetCoordinate) == null) {
-                            if (this.getPiece(coordinateWithSameColor).getCode().equals("b") || this.getPiece(coordinateWithSameColor).getCode().equals("n"))
-                                possibleCoordinatesToRemove.add(0, coordinateWithSameColor);
-                        }
+                    this.storePossibleCoordinates(possibleCoordinatesToRemove, coordinateWithSameColor, diagonalCoordinates);
                 }
         }
+    }
+
+    private void storePossibleCoordinates(List<Coordinate> possibleCoordinatesToRemove, Coordinate coordinateWithSameColor, List<Coordinate> diagonalCoordinates) {
+        for (Coordinate targetCoordinate : diagonalCoordinates)
+            if (this.isCorrectPairMove(0, coordinateWithSameColor, targetCoordinate) == null) {
+                if (this.getPiece(coordinateWithSameColor).getCode().equals("b") || this.getPiece(coordinateWithSameColor).getCode().equals("n"))
+                    possibleCoordinatesToRemove.add(0, coordinateWithSameColor);
+            }
     }
 
     private Error isCorrectPairMove(int pair, Coordinate... coordinates) {
